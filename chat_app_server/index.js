34870c1fd0d5e786d67bfdda188d7c1260e35905
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2020-10-09 14:34:46 -0500

    get usuarios DB

--- chat_app_server/index.js
+++ chat_app_server/index.js
@@ -26,6 +26,7 @@ app.use( express.static( publicPath ) );
 
 // mis rutas
 app.use("/api/login", require("./routes/auth"));
+app.use("/api/usuarios", require("./routes/usuario"));
 
 
 
--- chat_app_server/sockets/socket.js
+++ chat_app_server/sockets/socket.js
@@ -1,9 +1,13 @@
 const { comprobarJWT } = require('../helpers/jwt');
 const { io } = require('../index');
+const { usuarioConectado, usuarioDesconectado } = require('../controllers/socket');
+
 
 
 // Mensajes de Sockets
-io.on('connection', client => {    
+io.on('connection',   client => {    
+
+    // verificar autentificado
     const [valido,uid]= comprobarJWT(client.handshake.headers["x-token"]);
     if(!valido){
         console.log('Rechazado');
@@ -11,10 +15,13 @@ io.on('connection', client => {
        
     }
     console.log('Cliente Conectado--------');
-    
+    //cliente autentificado
+    usuarioConectado(uid);
+
 
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
+        usuarioDesconectado(uid);
     });
 
     /*client.on('mensaje', ( payload ) => {
